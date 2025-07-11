5dd1da3925009ce7f64cd13a61b316a8cb74f119
Chamana <gustavo.chamana@gmail.com>
2025-07-11 12:41:35 -0500

    Initial commit

---
+++ src/main/java/com/example/stepstyleshop/StepstyleShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.stepstyleshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StepstyleShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StepstyleShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/stepstyleshop/StepstyleShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stepstyleshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StepstyleShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
